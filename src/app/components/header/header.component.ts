f4eb909786623e8f7f01ecada9d052012c12dab8
rechegoy <rebeca.echegoy@gmail.com>
2024-01-29 21:08:19 -0300

    Agregado de ngClass al home para visualizar diferente ícono si está colapsado o no el menú Agregado de estilos al toggle icon Importación de is collapse

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink } from '@angular/router';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbCollapseModule, RouterLink],
+  imports: [NgbCollapseModule, RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  isMenuCollapsed = true;
+  isMenuCollapsed: boolean = true;
 }
