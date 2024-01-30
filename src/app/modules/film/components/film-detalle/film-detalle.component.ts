af46172d55fd285aded32289ce03292644e8e054
rechegoy <rebeca.echegoy@gmail.com>
2024-01-29 21:11:13 -0300

    Modificación de card del detalle de la búsqueda Agregado de loader Agregado de funcionalidad volver manteniendo la búsqueda realizada

--- src/app/modules/film/components/film-detalle/film-detalle.component.ts
+++ src/app/modules/film/components/film-detalle/film-detalle.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {OmdbService} from "../../../../services/omdb.service";
-import {Film} from "../../../../interfaces/film";
+import {JsonPipe, Location} from '@angular/common';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { OmdbService } from "../../../../services/omdb.service";
+import { Film } from "../../../../interfaces/film";
 
 @Component({
   selector: 'app-film-detalle',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    JsonPipe
+  ],
   templateUrl: './film-detalle.component.html',
   styleUrl: './film-detalle.component.scss'
 })
 export class FilmDetalleComponent implements OnInit {
   film: Film | undefined;
+  loading: boolean = false;
   constructor(
     private route: ActivatedRoute,
-    private omdbService: OmdbService
+    private omdbService: OmdbService,
+    private location: Location
   ) { }
 
   ngOnInit(): void {
+   this.loading = true;
     const idimdbID: string = this.route.snapshot.paramMap.get('idimdbID') || '';
-
     if ( idimdbID ) {
       this.omdbService.getFilm( idimdbID ).subscribe({
         next: ( response: Film ) => {
           this.film = response;
+          this.loading = false;
         }
       });
     }
+  }
+
+  back(): void {
+    this.location.back();
   }
 }
