621effa635eaadbf75f6d9b159d6e8fec157e6d5
rechegoy <rebeca.echegoy@gmail.com>
2024-01-29 21:05:48 -0300

    Agregado de archivo config para utilizarlo en pipi

---
+++ src/app/config.ts
@@ -0,0 +1,6 @@
+export const types: { value: string, label: string }[] = [
+  { value: '', label: 'Todos' },
+  { value: 'movie', label: 'Pel\u00edcula' },
+  { value: 'series', label: 'Series' },
+  { value: 'episode', label: 'Episodio' }
+];
